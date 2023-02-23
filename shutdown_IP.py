2b2ca2cc2b5b623007110f75ca05e4853c9351e8
Anderson Camargo <anderson101214@hotmail.com>
2023-02-23 20:01:09 -0300

    1

--- shutdown_IP.py
+++ shutdown_IP.py
@@ -4,15 +4,16 @@ print("--------------- DESLIGAR MAQUINAS ---------------")
 
 
 name = input("Digite o numero da sala: ")
-numbermax = int(input("Digite o numero de maquinas da sala: "))
+numbermax = int(input("Digite o numero de maquinas da sala: ")) #até qual maquina desligar
 contador = 1 #aparti de qual maquina irá começar
 
 while contador <= numbermax:
     txt = os.system (f"shutdown /m \\\\10.10.{name}.{contador} /s /f") 
     # os.system(f"shutdown /m \\\\10.10.{name}.{contador} /s /f")
     if txt == "shutdown /m \\\\10.10.{name}.{contador} /s /f":
-        print('maquina desligada com sucesso')
+        print(f'maquina 10.10.{name}.{contador} com sucesso')
     else:
+        os.system('cls')
         print(f'maquina 10.10.{name}.{contador} não encontrada na rede')
 
     contador += 1
