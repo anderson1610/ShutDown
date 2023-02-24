d742dfe3d6ddd33c87c70d86c6e9399bd1dd5398
Anderson Camargo <anderson101214@hotmail.com>
2023-02-23 21:33:02 -0300

    log
    Salvar log junto com a data e hora do sistema

--- shutdown_IP.py
+++ shutdown_IP.py
@@ -1,9 +1,13 @@
 import os
 from pathlib import Path
+from datetime import date, datetime
 
 print("--------------- DESLIGAR MAQUINAS ---------------")
 
-LOG_FILE = Path(__file__).parent  / "log.txt"
+date_today = date.today()
+# date_e_clock_today = datetime.now()
+# date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
+LOG_FILE = Path(__file__).parent  / (f"log_{date_today}.txt")
 name = input("Digite o numero da sala: ")
 numbermax = int(input("Digite o numero de maquinas da sala: ")) #até qual maquina desligar
 contador = 1 #aparti de qual maquina irá começar
@@ -13,11 +17,15 @@ while contador <= numbermax:
     # os.system(f"shutdown /m \\\\10.10.{name}.{contador} /s /f")
     if txt == "shutdown /m \\\\10.10.{name}.{contador} /s /f":
         print(f'maquina 10.10.{name}.{contador} desligada com sucesso')
-        txt2 = (f'maquina 10.10.{name}.{contador} desligada com sucesso')
+        date_e_clock_today = datetime.now()
+        date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
+        txt2 = (f'maquina 10.10.{name}.{contador} desligada com sucesso {date_e_clock_today}')
     else:
         #os.system('cls')
         print(f'maquina 10.10.{name}.{contador} não encontrada na rede')
-        txt2 = (f'maquina 10.10.{name}.{contador} não encontrada na rede | Maquina ja desligada')
+        date_e_clock_today = datetime.now()
+        date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
+        txt2 = (f'maquina 10.10.{name}.{contador} não encontrada na rede | Maquina ja desligada {date_e_clock_today}')
     
     with open (LOG_FILE, "a", encoding='utf8') as arquivo:
             arquivo.write(txt2)
