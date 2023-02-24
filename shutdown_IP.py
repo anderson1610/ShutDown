911432653d83ae1c34b16c0fc4c48b86f1c30761
Anderson Camargo <anderson101214@hotmail.com>
2023-02-23 21:11:12 -0300

    Logs
    Adicionei funcionalidades na qual o script registra em um TXT os logs de atividade

--- shutdown_IP.py
+++ shutdown_IP.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 print("--------------- DESLIGAR MAQUINAS ---------------")
 
-
+LOG_FILE = Path(__file__).parent  / "log.txt"
 name = input("Digite o numero da sala: ")
 numbermax = int(input("Digite o numero de maquinas da sala: ")) #até qual maquina desligar
 contador = 1 #aparti de qual maquina irá começar
@@ -11,10 +12,16 @@ while contador <= numbermax:
     txt = os.system (f"shutdown /m \\\\10.10.{name}.{contador} /s /f") 
     # os.system(f"shutdown /m \\\\10.10.{name}.{contador} /s /f")
     if txt == "shutdown /m \\\\10.10.{name}.{contador} /s /f":
-        print(f'maquina 10.10.{name}.{contador} com sucesso')
+        print(f'maquina 10.10.{name}.{contador} desligada com sucesso')
+        txt2 = (f'maquina 10.10.{name}.{contador} desligada com sucesso')
     else:
         #os.system('cls')
         print(f'maquina 10.10.{name}.{contador} não encontrada na rede')
+        txt2 = (f'maquina 10.10.{name}.{contador} não encontrada na rede | Maquina ja desligada')
+    
+    with open (LOG_FILE, "a", encoding='utf8') as arquivo:
+            arquivo.write(txt2)
+            arquivo.write("\n")
 
     contador += 1
 
