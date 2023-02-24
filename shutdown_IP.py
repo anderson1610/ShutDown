fe4033d85680de4b591c25468b216c61e877e315
Anderson Camargo <anderson101214@hotmail.com>
2023-02-24 19:19:31 -0300

    Update
    Tive que alterar a forma de obtenção de resposta do console, para verificar se a função obteve ou não uma resposta positiva.

--- shutdown_IP.py
+++ shutdown_IP.py
@@ -1,41 +1,37 @@
 import os
 from pathlib import Path
 from datetime import date, datetime
+import subprocess
 
 print("--------------- DESLIGAR MAQUINAS ---------------")
 
 date_today = date.today()
-# date_e_clock_today = datetime.now()
-# date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
-LOG_FILE = Path(__file__).parent  / (f"log_{date_today}.txt")
 name = input("Digite o numero da sala: ")
+LOG_FILE = Path(__file__).parent  / (f"Relatorio_Maquinas_S{name}_{date_today}.txt")
 numbermax = int(input("Digite o numero de maquinas da sala: ")) #até qual maquina desligar
 contador = 1 #aparti de qual maquina irá começar
 
+
+def ping(sala, maquina):
+    date_e_clock_today = datetime.now()
+    date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
+    txt = subprocess.run(f"shutdown /m \\\\10.10.{sala}.{maquina} /s /f /t 6", stdout=subprocess.PIPE) 
+    stdout = txt.stdout
+    return f"maquina 10.10.{sala}.{maquina} desligada com sucesso {date_e_clock} " if txt.returncode == 0 else f'maquina 10.10.{sala}.{maquina} não encontrada | Maquina ja desligada {date_e_clock}'
+
 while contador <= numbermax:
-    txt = os.system (f"shutdown /m \\\\10.10.{name}.{contador} /s /f") 
-    # os.system(f"shutdown /m \\\\10.10.{name}.{contador} /s /f")
-    if txt == "shutdown /m \\\\10.10.{name}.{contador} /s /f":
-        print(f'maquina 10.10.{name}.{contador} desligada com sucesso')
-        date_e_clock_today = datetime.now()
-        date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
-        txt2 = (f'maquina 10.10.{name}.{contador} desligada com sucesso {date_e_clock_today}')
-    else:
-        #os.system('cls')
-        print(f'maquina 10.10.{name}.{contador} não encontrada na rede')
-        date_e_clock_today = datetime.now()
-        date_e_clock = date_e_clock_today.strftime('%d/%m/%Y %H:%M')
-        txt2 = (f'maquina 10.10.{name}.{contador} não encontrada na rede | Maquina ja desligada {date_e_clock_today}')
-    
+    pings = [ping(name, contador)]
+    print("salvando arquivo..")
+
     with open (LOG_FILE, "a", encoding='utf8') as arquivo:
-            arquivo.write(txt2)
-            arquivo.write("\n")
+                for p in pings: 
+                    arquivo.write(f'{p}\n')
 
     contador += 1
 
-
-
-
+# os.system(f"shutdown /m \\\\10.10.{name}.200 /s /f")
+# with open (LOG_FILE, "a", encoding='utf8') as arquivo:
+#       arquivo.write(f'Maquina 10.10.{name}.200, instrutor desligada')
 
 
 #Desenvol. Anderson Camargo
